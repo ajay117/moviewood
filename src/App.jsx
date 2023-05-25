1add1093a5681ab49a33d0c728f7b050d82e1695
Ajay KC <meajay64@gmail.com>
2023-05-25 13:48:33 +0900

    Make movies fetch work

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
+import CenteredTabs from "./components/CenteredTabs";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import MoviesMediaCard from "./components/MoviesMediaCard";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div>Hello World</div>;
+  const [show, setShow] = useState("movies");
+  const [moviesList, setMoviesList] = useState([]);
+  const [tab, setTab] = useState("now playing");
+  // const tabs = ["now playing", "popular", "top rated", "upcoming", "trending"];
+
+  const changeTab = (event) => {
+    setTab(event.target.innerText.toLowerCase());
+  };
+
+  useEffect(() => {
+    let tabParam = tab;
+
+    if (tab === "now playing") {
+      tabParam = "now_playing";
+    } else if (tab === "top rated") {
+      tabParam = "top_rated";
+    }
+
+    let url = `https://api.themoviedb.org/3/movie/${tabParam}?language=en-US&page=1`;
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer " + import.meta.env.VITE_AUTH_TOKEN,
+      },
+    };
+
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((response) => setMoviesList(response.results))
+      .catch((err) => console.error(err));
+  }, [tab]);
+
+  return (
+    <div>
+      <Container maxWidth="md">
+        <CenteredTabs changeTab={changeTab} />
+        <Grid container spacing={2}>
+          {show === "movies" && (
+            <>
+              {moviesList.map((movie, index) => (
+                <Grid key={index} item sm={6} xs={12}>
+                  <MoviesMediaCard movie={movie} />
+                </Grid>
+              ))}
+            </>
+          )}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
