a9a537da7ea135d8048c682933de0d3950d3c11f
Ajay KC <meajay64@gmail.com>
2023-05-26 22:51:28 +0900

    Add logo

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,26 +1,33 @@
 import PropTypes from "prop-types";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import logo from "../assets/logo.jpeg";
 
 export default function MenuBar({ handleClick }) {
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: "flex-end",
+        justifyContent: "space-between",
         alignItems: "center",
         textAlign: "center",
+        padding: "0 20px",
       }}
     >
-      <Button onClick={handleClick} variant="text">
-        movie
-      </Button>
-      <Button onClick={handleClick} variant="text">
-        tv
-      </Button>
-      <Button onClick={handleClick} variant="text">
-        person
-      </Button>
+      <div>
+        <img className="logo"  src={logo} alt=""></img>
+      </div>
+      <div>
+        <Button onClick={handleClick} variant="text">
+          movie
+        </Button>
+        <Button onClick={handleClick} variant="text">
+          tv
+        </Button>
+        <Button onClick={handleClick} variant="text">
+          person
+        </Button>
+      </div>
     </Box>
   );
 }
