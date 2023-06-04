49bbfface993face9524f0344f6856021d45f0f8
Ajay KC <meajay64@gmail.com>
2023-06-04 17:29:14 +0900

    Add teaser and trailers of movie

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -3,22 +3,28 @@ import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import { Divider } from "@mui/material";
 
 function MovieInfo() {
   let { id } = useParams();
   const [movieDetails, setMovieDetails] = useState([]);
+  const [videos, setVideos] = useState([]);
+  const controller = new AbortController();
+  const signal = controller.signal;
+  const videoLink = "https://www.youtube.com/watch?v=";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + import.meta.env.VITE_AUTH_TOKEN,
+    },
+    signal: signal,
+  };
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: "Bearer " + import.meta.env.VITE_AUTH_TOKEN,
-      },
-      signal: signal,
-    };
 
     fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, options)
       .then((response) => response.json())
@@ -26,14 +32,25 @@ function MovieInfo() {
       .catch((err) => console.error(err));
   }, []);
 
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setVideos(response.results))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <Container sx={{padding: "20px"}} maxWidth="md">
-      {console.log(movieDetails)}
+    <Container sx={{ padding: "20px" }} maxWidth="md">
+      {/* {console.log(movieDetails)} */}
+      {console.log(videos)}
       <h2 className="text-center">{movieDetails.title}</h2>
       {/* <h3>ID: {id} </h3> */}
 
       <div className="box">
-        <div  className="img">
+        <div className="img">
           <img
             src={"https://image.tmdb.org/t/p/w500" + movieDetails.poster_path}
             alt=""
@@ -61,8 +78,28 @@ function MovieInfo() {
           </p>
         </div>
       </div>
+
+      <h3>Videos:</h3>
+      <div className="grid-container">
+        {videos.map(
+          (item, index) =>
+            index < 5 && (
+              <div key={item.id} className="grid-container_child">
+                <iframe
+                  src={`https://www.youtube.com/embed/${item.key}`}
+                ></iframe>
+                <p>
+                  <b>Type: </b>
+                  {item.type}
+                </p>
+                <Divider sx={{borderWidth: 2}} />
+              </div>
+            )
+        )}
+      </div>
     </Container>
   );
 }
 
 export default MovieInfo;
+
