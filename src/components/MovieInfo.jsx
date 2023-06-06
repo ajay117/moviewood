be4045e0f241af0b006415b868aa195dcd015693
Ajay KC <meajay64@gmail.com>
2023-06-06 16:49:24 +0900

    Add header, go home link and format money

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Divider } from "@mui/material";
+import logo from "../assets/logo.jpeg";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import Header from "./Header";
+import formatNumber from "../modules/formatNumber";
 
 function MovieInfo() {
   let { id } = useParams();
@@ -38,61 +43,66 @@ function MovieInfo() {
       .then((response) => setVideos(response.results))
       .catch((err) => console.error(err));
   }, []);
+  const budget = movieDetails.budget;
 
   return (
-    <Container sx={{ padding: "20px" }} maxWidth="md">
-      <h2 className="text-center">{movieDetails.title}</h2>
+    <div>
+      <Header />
 
-      <div className="box">
-        <div className="img">
-          <img
-            src={"https://image.tmdb.org/t/p/w500" + movieDetails.poster_path}
-            alt=""
-            style={{ maxHeight: "300px" }}
-          />
+      <Container sx={{ padding: "20px" }} maxWidth="md">
+        <h2 className="text-center">{movieDetails.title}</h2>
+
+        <div className="box">
+          <div className="img">
+            <img
+              src={"https://image.tmdb.org/t/p/w500" + movieDetails.poster_path}
+              alt=""
+              style={{ maxHeight: "300px" }}
+            />
+          </div>
+
+          <div>
+            <p>{movieDetails.overview}</p>
+            <p>
+              <b>Homepage:</b>{" "}
+              <a href={movieDetails.homepage} target="_blank">
+                {movieDetails.homepage}
+              </a>
+            </p>
+            <p>
+              <b>Budget:</b> ${formatNumber(budget)}
+            </p>
+
+            <p>
+              <b>Release Date:-</b> {movieDetails.release_date}
+            </p>
+            <p>
+              <b>Rating:</b> {movieDetails.vote_average}
+            </p>
+          </div>
         </div>
 
-        <div>
-          <p>{movieDetails.overview}</p>
-          <p>
-            <b>Homepage:</b>{" "}
-            <a href={movieDetails.homepage} target="_blank">
-              {movieDetails.homepage}
-            </a>
-          </p>
-          <p>
-            <b>Budget:</b> ${movieDetails.budget}
-          </p>
-
-          <p>
-            <b>Release Date:-</b> {movieDetails.release_date}
-          </p>
-          <p>
-            <b>Rating:</b> {movieDetails.vote_average}
-          </p>
+        <h3 style={{ textAlign: "center" }}>Related videos:</h3>
+        <div className="grid-container">
+          {videos.map(
+            (item, index) =>
+              index < 5 && (
+                <div
+                  style={{ border: "1px solid black", padding: "20px" }}
+                  key={item.id}
+                  className="grid-container_child"
+                >
+                  <iframe
+                    src={`https://www.youtube.com/embed/${item.key}`}
+                  ></iframe>
+                  <p>{item.name}</p>
+                  <Divider sx={{ borderWidth: 2 }} />
+                </div>
+              )
+          )}
         </div>
-      </div>
-
-      <h3 style={{ textAlign: "center" }}>Related videos:</h3>
-      <div className="grid-container">
-        {videos.map(
-          (item, index) =>
-            index < 5 && (
-              <div
-                style={{ border: "1px solid black", padding: "20px" }}
-                key={item.id}
-                className="grid-container_child"
-              >
-                <iframe
-                  src={`https://www.youtube.com/embed/${item.key}`}
-                ></iframe>
-                <p>{item.name}</p>
-                <Divider sx={{ borderWidth: 2 }} />
-              </div>
-            )
-        )}
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 }
 
