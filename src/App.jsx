76a97ff98d6b70aba73e2489a0fe63e1622d809c
Ajay KC <meajay64@gmail.com>
2023-06-09 22:49:06 +0900

    Add new route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import MovieInfo from "./components/MovieInfo";
 import TvInfo from "./components/TvInfo";
+import PeopleInfo from "./components/PeopleInfo";
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieInfo />} />
         <Route path="/tvshow/:id" element={<TvInfo />} />
+        <Route path="/people/:id" element={<PeopleInfo />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
