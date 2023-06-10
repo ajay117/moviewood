2cf048e40cda56433a2d0443bab251cf3de3480e
Ajay KC <meajay64@gmail.com>
2023-06-10 09:41:01 +0900

    Add tooltip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo.jpeg";
 import Button from "@mui/material/Button";
 import HomeIcon from "@mui/icons-material/Home";
 import { Link } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
 function Header() {
   return (
@@ -18,12 +19,15 @@ function Header() {
       <Box sx={{ width: { xs: "150px", lg: "180px" } }}>
         <img className="logo" src={logo} alt="" />
       </Box>
-      <Button variant="text" sx={{ fontSize: { xs: "12px", sm: "15px" } }}>
-        <Link to="/" style={{ display: "flex" }}>
-          <HomeIcon style={{ marginRight: "1px" }} />
-          Go Home
-        </Link>
-      </Button>
+
+      <Tooltip title="Go back to home page" arrow>
+        <Button variant="text" sx={{ fontSize: { xs: "12px", sm: "15px" } }}>
+          <Link to="/" style={{ display: "flex" }}>
+            <HomeIcon style={{ marginRight: "1px" }} />
+            Go Home
+          </Link>
+        </Button>
+      </Tooltip>
     </Box>
   );
 }
