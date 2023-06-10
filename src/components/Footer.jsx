74f7dcdd92e5351d5b54b92129bc347a25a9b491
Ajay KC <meajay64@gmail.com>
2023-06-10 09:40:49 +0900

    Change footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { Tooltip } from "@mui/material";
+import { useLocation } from "react-router-dom";
 
 function Footer() {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
@@ -14,32 +17,37 @@ function Footer() {
 
   return (
     <footer style={{ marginTop: "20px" }}>
-      <div className="photo-credit">
-        Thanks to the HeroSection image by{" "}
-        <a
-          target="_blank"
-          href="https://unsplash.com/photos/AtPWnYNDJnM?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
-        >
-          Krists Luhaers on unsplash
-        </a>
-      </div>
+      {isHomePage && (
+        <div className="photo-credit">
+          Photo credit:{" "}
+          <a
+            target="_blank"
+            href="https://unsplash.com/photos/AtPWnYNDJnM?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink"
+            rel="noopener noreferrer"
+          >
+            Krists Luhaers on Unsplash
+          </a>
+        </div>
+      )}
       <p>
         Developed by{" "}
         <Tooltip title="Go to developer website" arrow>
           <a
             href="https://ajaykc.netlify.app/"
             target="_blank"
+            rel="noopener noreferrer"
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
             style={{
               color: isHovered ? "#4135dc" : "inherit",
               textDecoration: isHovered ? "underline" : "none",
+              fontWeight: "bold",
             }}
           >
             Ajay KC
           </a>
         </Tooltip>
-        .
+        . All rights reserved.
       </p>
     </footer>
   );
