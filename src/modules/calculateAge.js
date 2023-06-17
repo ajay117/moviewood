6159d7c6b7d2b0ca118f763c51b66342f1f5a82a
Ajay KC <meajay64@gmail.com>
2023-06-18 00:43:37 +0900

    Add helper function to calculate age

---
+++ src/modules/calculateAge.js
@@ -0,0 +1,16 @@
+function calculateAge(dateOfBirth, dateOfDeath) {
+    const today = dateOfDeath ? new Date(dateOfDeath) : new Date();
+    const birthDate = new Date(dateOfBirth);
+  
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+  
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+  
+    return age;
+  }
+  
+
+export default calculateAge;
